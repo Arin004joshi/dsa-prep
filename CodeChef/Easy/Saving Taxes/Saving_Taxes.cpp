87d90d1a0324612080a14e0ba853bb42542e9ba4
arin <139484606+Arin004joshi@users.noreply.github.com>
2025-08-25 19:27:10 +0530

    Add solution: Saving Taxes (CfSoln)

---
+++ CodeChef/Easy/Saving Taxes/Saving_Taxes.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int T;
+    cin >> T;
+    while (T--) {
+        long long X, Y;
+        cin >> X >> Y;
+        cout << (X - Y) << "\n";
+    }
+    return 0;
+}
